15b5fd86e381b209d82620743f04b1f0ba56c7e2
Sidney Kochman <sidney@repl.it>
2020-08-18 11:53:54 -0400

    set up Semaphore for CI (#2)

--- index.test.js
+++ index.test.js
@@ -1,53 +1,66 @@
-const Client = require('./index')
+const fetch = require("node-fetch");
+const Client = require("./index");
 
-const client = new Client();
+let client;
 
-client.empty();
+beforeAll(async () => {
+  const pass = process.env.PASSWORD;
+  const resp = await fetch("https://database-test-jwt.kochman.repl.co", {
+    headers: {
+      Authorization: "Basic " + btoa("test:" + pass),
+    },
+  });
+  const url = await resp.text();
+  client = new Client(url);
+  await client.empty();
+});
 
 afterEach(async () => {
   await client.empty();
-})
+});
 
-test('create a client with a key', async () => {
+test("create a client with a key", async () => {
   expect(client).toBeTruthy();
-  expect(typeof client.key).toBe('string');
-
+  expect(typeof client.key).toBe("string");
 });
 
-test('sets a value', async () => {
+test("sets a value", async () => {
   expect(await client.set("key", "value")).toEqual(client);
-  expect(await client.setAll({key: "value", second: "secondThing"})).toEqual(client);
-
+  expect(await client.setAll({ key: "value", second: "secondThing" })).toEqual(
+    client
+  );
 });
 
-test('list keys', async () => {
+test("list keys", async () => {
   await client.setAll({
     key: "value",
-    second: "secondThing"
-  })
-
-  expect(await client.list()).toEqual("key\nsecond")
-});
-
-test('gets a value', async () => {
-  await client.setAll({
-    key: "value"
+    second: "secondThing",
   });
 
-  expect(await client.getAll()).toEqual({key: "value"});
+  expect(await client.list()).toEqual("key\nsecond");
 });
 
-test('delete a value', async () => {
+test("gets a value", async () => {
+  await client.setAll({
+    key: "value",
+  });
+
+  expect(await client.getAll()).toEqual({ key: "value" });
+});
+
+test("delete a value", async () => {
   await client.setAll({
     key: "value",
     deleteThis: "please",
     somethingElse: "in delete multiple",
-    andAnother: "again same thing"
-  })
+    andAnother: "again same thing",
+  });
 
-  expect(await client.delete('deleteThis')).toEqual(client);
-  expect(await client.deleteMultiple('somethingElse', 'andAnother')).toEqual(client);
-  expect(await client.list()).toEqual("key")
+  expect(await client.delete("deleteThis")).toEqual(client);
+  expect(await client.deleteMultiple("somethingElse", "andAnother")).toEqual(
+    client
+  );
+  expect(await client.list()).toEqual("key");
   expect(await client.empty()).toEqual(client);
   expect(await client.list()).toEqual("");
-})
+});
