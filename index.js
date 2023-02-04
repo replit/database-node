5e70197b1e947d810ab378d3a4d6c3c5d5aa2dc1
Mech <83923848+7heMech@users.noreply.github.com>
2023-02-04 08:13:09 +0000

    Formatted the replit way xD

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 const https = require('https');
 
 const httpAgent = new http.Agent({
-  keepAlive: true
+	keepAlive: true
 });
 const httpsAgent = new https.Agent({
-  keepAlive: true
+	keepAlive: true
 });
 
 const agent = (_parsedURL) => _parsedURL.protocol == 'http:' ? httpAgent : httpsAgent;
@@ -14,212 +14,212 @@ const rawFetch = typeof fetch === 'undefined' ? require("./fetch.cjs") : fetch;
 
 // Keep alive for 2x faster requests
 const request = (...args) => {
-  args[1] = typeof options !== 'object' ? {
-    agent
-  } : {
-    ...args[1],
-    agent
-  };
+	args[1] = typeof options !== 'object' ? {
+		agent
+	} : {
+		...args[1],
+		agent
+	};
 
-  return rawFetch(...args);
+	return rawFetch(...args);
 };
 
 class CacheMap extends Map {
-  constructor(ms) {
-    super();
-    this.expiration = new Map();
+	constructor(ms) {
+		super();
+		this.expiration = new Map();
 		this.expiration.ms = ms;
-  }
-  get(key) {
-    const time = new Date().getTime(),
-      expiresAt = this.expiration.get(key);
+	}
+	get(key) {
+		const time = new Date().getTime(),
+			expiresAt = this.expiration.get(key);
 
-    let value = super.get(key);
+		let value = super.get(key);
 
-    if (time > expiresAt) {
-      value = null;
-      this.delete(key);
-      this.expiration.delete(key);
-    }
+		if (time > expiresAt) {
+			value = null;
+			this.delete(key);
+			this.expiration.delete(key);
+		}
 
-    return value;
-  }
-  set(key, value) {
-    const expiresAt = new Date().getTime() + this.expiration.ms;
-    this.expiration.set(key, expiresAt);
-    return super.set(key, value);
-  }
+		return value;
+	}
+	set(key, value) {
+		const expiresAt = new Date().getTime() + this.expiration.ms;
+		this.expiration.set(key, expiresAt);
+		return super.set(key, value);
+	}
 }
 
 class Client {
 	#url;
-  /**
-   * Initiates Class.
-   * @param {String} url Custom database URL
-   * @param {Number} [ms=1000*60*5] Milliseconds till cache expires
-   */
-  constructor(url, ms = 1000 * 60 * 5) {
-    this.cache = new CacheMap(ms);
-    this.#url = url ?
-      url :
-      process.env.REPLIT_DB_URL;
-    if (!this.#url) throw new Error("You must either pass a database URL into the Client constructor, or you must set the REPLIT_DB_URL environment variable. If you are using the repl.it editor, you must log in to get an auto-generated REPLIT_DB_URL environment variable.");
-  }
+	/**
+	 * Initiates Class.
+	 * @param {String} url Custom database URL
+	 * @param {Number} [ms=1000*60*5] Milliseconds till cache expires
+	 */
+	constructor(url, ms = 1000 * 60 * 5) {
+		this.cache = new CacheMap(ms);
+		this.#url = url ?
+			url :
+			process.env.REPLIT_DB_URL;
+		if (!this.#url) throw new Error("You must either pass a database URL into the Client constructor, or you must set the REPLIT_DB_URL environment variable. If you are using the repl.it editor, you must log in to get an auto-generated REPLIT_DB_URL environment variable.");
+	}
 
-  // Native Functions
-  /**
-   * Gets a key
-   * @param {String} key Key
-   * @param {boolean} [options.raw=false] Makes it so that we return the raw string value. Default is false.
-   */
-  async get(key, options) {
-    const value = this.cache.get(key) ?? await this.fetch(key);
+	// Native Functions
+	/**
+	 * Gets a key
+	 * @param {String} key Key
+	 * @param {boolean} [options.raw=false] Makes it so that we return the raw string value. Default is false.
+	 */
+	async get(key, options) {
+		const value = this.cache.get(key) ?? await this.fetch(key);
 
-    return options?.raw ?
-      value :
-      JSON.parse(value) ?? null;
-  }
+		return options?.raw ?
+			value :
+			JSON.parse(value) ?? null;
+	}
 
-  /**
-   * Fetches a key
-   * @param {String} key Key
-   * @param {boolean} [options.raw=false] Makes it so that we return the raw string value. Default is false.
-   */
-  async fetch(key, options) {
-    const res = await request(`${this.#url}/${key}`);
-    const value = await res.text();
+	/**
+	 * Fetches a key
+	 * @param {String} key Key
+	 * @param {boolean} [options.raw=false] Makes it so that we return the raw string value. Default is false.
+	 */
+	async fetch(key, options) {
+		const res = await request(`${this.#url}/${key}`);
+		const value = await res.text();
 
-    this.cache.set(key, value);
+		this.cache.set(key, value);
 
-    return options?.raw ?
-      value :
-      JSON.parse(value) ?? null;
-  }
+		return options?.raw ?
+			value :
+			JSON.parse(value) ?? null;
+	}
 
-  /**
-   * Sets a key
-   * @param {String} key Key
-   * @param {any} value Value
-   */
-  async set(key, value) {
-    const strValue = JSON.stringify(value);
+	/**
+	 * Sets a key
+	 * @param {String} key Key
+	 * @param {any} value Value
+	 */
+	async set(key, value) {
+		const strValue = JSON.stringify(value);
 
-    this.cache.set(key, strValue);
+		this.cache.set(key, strValue);
 
-    await request(this.#url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: `${encodeURIComponent(key)}=${encodeURIComponent(strValue)}`,
-    });
-    return this;
-  }
+		await request(this.#url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: `${encodeURIComponent(key)}=${encodeURIComponent(strValue)}`,
+		});
+		return this;
+	}
 
-  /**
-   * Deletes a key
-   * @param {String} key Key
-   */
-  async delete(key) {
-    this.cache.delete(key);
-    await request(`${this.#url}/${key}`, {
-      method: "DELETE"
-    });
-    return this;
-  }
+	/**
+	 * Deletes a key
+	 * @param {String} key Key
+	 */
+	async delete(key) {
+		this.cache.delete(key);
+		await request(`${this.#url}/${key}`, {
+			method: "DELETE"
+		});
+		return this;
+	}
 
-  /**
-   * List keys starting with a prefix or list all from cache or if none in cache from db.
-   * @param {String} prefix Filter keys starting with prefix.
-   */
-  async list(prefix = "") {
-    let keys = [...this.cache.keys()].filter(key => key.startsWith(prefix));
-    if (!keys.length === 0) keys = await this.fetchList(prefix);
-    return keys;
-  }
+	/**
+	 * List keys starting with a prefix or list all from cache or if none in cache from db.
+	 * @param {String} prefix Filter keys starting with prefix.
+	 */
+	async list(prefix = "") {
+		let keys = [...this.cache.keys()].filter(key => key.startsWith(prefix));
+		if (!keys.length === 0) keys = await this.fetchList(prefix);
+		return keys;
+	}
 
-  /**
-   * List key starting with a prefix or list all.
-   * @param {String} prefix Filter keys starting with prefix.
-   */
-  async fetchList(prefix = "") {
-    const res = await request(
-      `${this.#url}?encode=true&prefix=${encodeURIComponent(prefix)}`
-    );
-    const text = await res.text();
+	/**
+	 * List key starting with a prefix or list all.
+	 * @param {String} prefix Filter keys starting with prefix.
+	 */
+	async fetchList(prefix = "") {
+		const res = await request(
+			`${this.#url}?encode=true&prefix=${encodeURIComponent(prefix)}`
+		);
+		const text = await res.text();
 
-    if (text.length === 0) return [];
-    return text.split("\n").map(decodeURIComponent);
-  }
+		if (text.length === 0) return [];
+		return text.split("\n").map(decodeURIComponent);
+	}
 
-  // Dynamic Functions
-  /**
-   * Clears the database.
-   */
-  async empty() {
-    const promises = [];
-    for (const key of await this.fetchList()) {
-      promises.push(this.delete(key));
-    }
+	// Dynamic Functions
+	/**
+	 * Clears the database.
+	 */
+	async empty() {
+		const promises = [];
+		for (const key of await this.fetchList()) {
+			promises.push(this.delete(key));
+		}
 
-    await Promise.all(promises);
+		await Promise.all(promises);
 
-    return this;
-  }
+		return this;
+	}
 
-  /**
-   * Get all key/value pairs and return as an object.
-   */
-  async getAll() {
-    let output = {};
-    for (const key of await this.list()) {
-      let value = await this.get(key);
-      output[key] = value;
-    }
-    return output;
-  }
+	/**
+	 * Get all key/value pairs and return as an object.
+	 */
+	async getAll() {
+		let output = {};
+		for (const key of await this.list()) {
+			let value = await this.get(key);
+			output[key] = value;
+		}
+		return output;
+	}
 
-  /**
-   * Fetch all key/value pairs and return as an object.
-   */
-  async fetchAll() {
-    let output = {};
-    for (const key of await this.fetchList()) {
-      let value = await this.fetch(key);
-      output[key] = value;
-    }
-    return output;
-  }
+	/**
+	 * Fetch all key/value pairs and return as an object.
+	 */
+	async fetchAll() {
+		let output = {};
+		for (const key of await this.fetchList()) {
+			let value = await this.fetch(key);
+			output[key] = value;
+		}
+		return output;
+	}
 
-  /**
-   * Sets the entire database through an object.
-   * @param {Object} obj The object.
-   */
-  async setAll(obj) {
-    for (const key in obj) {
-      let val = obj[key];
-      await this.set(key, val);
-    }
-    return this;
-  }
+	/**
+	 * Sets the entire database through an object.
+	 * @param {Object} obj The object.
+	 */
+	async setAll(obj) {
+		for (const key in obj) {
+			let val = obj[key];
+			await this.set(key, val);
+		}
+		return this;
+	}
 
-  /**
-   * Delete multiple entries by keys
-   * @param {Array<string>} args Keys
-   */
-  async deleteMultiple(...args) {
-    const promises = [];
+	/**
+	 * Delete multiple entries by keys
+	 * @param {Array<string>} args Keys
+	 */
+	async deleteMultiple(...args) {
+		const promises = [];
 
-    for (const arg of args) {
-      promises.push(this.delete(arg));
-    }
+		for (const arg of args) {
+			promises.push(this.delete(arg));
+		}
 
-    await Promise.all(promises);
+		await Promise.all(promises);
 
-    return this;
-  }
+		return this;
+	}
 }
 
 module.exports = {
-  Client
+	Client
 };
--- index.test.js
+++ index.test.js
@@ -1,93 +1,93 @@
 const request = typeof fetch === 'undefined' ? require("./fetch.cjs") : fetch;
 const {
-  Client
+	Client
 } = require("./index");
 
 let client;
 
 beforeAll(async () => {
-  const pass = process.env.PASSWORD;
-  if (pass) {
-    const resp = await fetch("https://database-test-jwt.kochman.repl.co", {
-      headers: {
-        Authorization: "Basic " + btoa("test:" + pass),
-      },
-    });
-    const url = await resp.text();
-    client = new Client(url);
-  } else {
-    client = new Client();
-  }
+	const pass = process.env.PASSWORD;
+	if (pass) {
+		const resp = await fetch("https://database-test-jwt.kochman.repl.co", {
+			headers: {
+				Authorization: "Basic " + btoa("test:" + pass),
+			},
+		});
+		const url = await resp.text();
+		client = new Client(url);
+	} else {
+		client = new Client();
+	}
 
-  await client.empty();
+	await client.empty();
 });
 
 afterEach(async () => {
-  await client.empty();
+	await client.empty();
 });
 
 test("create a client with a key", async () => {
-  expect(client).toBeTruthy();
-  expect(typeof client.key).toBe("string");
+	expect(client).toBeTruthy();
+	expect(typeof client.key).toBe("string");
 });
 
 test("sets a value", async () => {
-  expect(await client.set("key", "value")).toEqual(client);
-  expect(await client.setAll({
-    key: "value",
-    second: "secondThing"
-  })).toEqual(
-    client
-  );
+	expect(await client.set("key", "value")).toEqual(client);
+	expect(await client.setAll({
+		key: "value",
+		second: "secondThing"
+	})).toEqual(
+		client
+	);
 });
 
 test("list keys", async () => {
-  await client.setAll({
-    key: "value",
-    second: "secondThing",
-  });
+	await client.setAll({
+		key: "value",
+		second: "secondThing",
+	});
 
-  expect(await client.list()).toEqual(["key", "second"]);
+	expect(await client.list()).toEqual(["key", "second"]);
 });
 
 test("gets a value", async () => {
-  await client.setAll({
-    key: "value",
-  });
+	await client.setAll({
+		key: "value",
+	});
 
-  expect(await client.getAll()).toEqual({
-    key: "value"
-  });
+	expect(await client.getAll()).toEqual({
+		key: "value"
+	});
 });
 
 test("delete a value", async () => {
-  await client.setAll({
-    key: "value",
-    deleteThis: "please",
-    somethingElse: "in delete multiple",
-    andAnother: "again same thing",
-  });
+	await client.setAll({
+		key: "value",
+		deleteThis: "please",
+		somethingElse: "in delete multiple",
+		andAnother: "again same thing",
+	});
 
-  expect(await client.delete("deleteThis")).toEqual(client);
-  expect(await client.deleteMultiple("somethingElse", "andAnother")).toEqual(
-    client
-  );
-  expect(await client.list()).toEqual(["key"]);
-  expect(await client.empty()).toEqual(client);
-  expect(await client.list()).toEqual([]);
+	expect(await client.delete("deleteThis")).toEqual(client);
+	expect(await client.deleteMultiple("somethingElse", "andAnother")).toEqual(
+		client
+	);
+	expect(await client.list()).toEqual(["key"]);
+	expect(await client.empty()).toEqual(client);
+	expect(await client.list()).toEqual([]);
 });
 
 test("list keys with newline", async () => {
-  await client.setAll({
-    "key\nwit": "first",
-    keywidout: "second",
-  });
+	await client.setAll({
+		"key\nwit": "first",
+		keywidout: "second",
+	});
 
-  expect(await client.list()).toEqual(["keywidout", "key\nwit"]);
+	expect(await client.list()).toEqual(["keywidout", "key\nwit"]);
 });
 
 test("ensure that we escape values when setting", async () => {
-  expect(await client.set("a", "1;b=2")).toEqual(client);
-  expect(await client.list()).toEqual(["a"])
-  expect(await client.get("a")).toEqual("1;b=2")
+	expect(await client.set("a", "1;b=2")).toEqual(client);
+	expect(await client.list()).toEqual(["a"])
+	expect(await client.get("a")).toEqual("1;b=2")
 });
